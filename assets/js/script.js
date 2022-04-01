e7a727171dd307a7db40114c97347b198cf91625
Andrew Garner <awgarner@gmail.com>
2022-04-01 15:37:34 -0400

    used innerHTML to write num to divEl. divEl uses querySelector to target the timer span by using #dynamic-timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
 // timer funcitonality
 
-let num = 60;
+// 60 is hard coded into the html as the starting value
+let num = 59;
 
+// function that dynamically prints num to html and subtracts 1 from num each time it runs
 function printNumber () {
-    // selects the html element with id of dynamic-timer
+        // selects the html element with id of dynamic-timer
     var divEl = document.querySelector("#dynamic-timer");
-    // creates variable timerEl and creates an html element with .createElement("")
-    var timerEl = document.createElement("span");
-    // sets text content of timerEl with the value in the variable num
-    timerEl.textContent = num;
-    // appends the contents of timerEl as a child to the div targeted with devEl variable
-    divEl.appendChild(timerEl);
-    // conditional statement that stops the setInterval helper funciton when num is 0
+        // creates variable timerEl and creates an html element with .createElement("span")
+    var timerNowEl = document.createElement("span");
+        // appends the innerHTML of divEl with the value of num
+    divEl.innerHTML = num;
+        // conditional statement that stops the setInterval helper funciton when num is 0
     if (num === 0) {
         clearInterval(interval);
     }
-    // subtracts one from num every time the printNumber() is called
+     // subtracts one from num 
     num--;
 }
 
 // helper function that takes in two parameters (function you want to run, # of milliseconds between runs)
-
+// runs printNumber() every second
 let interval = setInterval(printNumber, 1000);
