3886f9680f60acf51cef4168ea24108a6ff1450b
Andrew Garner <awgarner@gmail.com>
2022-04-03 11:43:02 -0400

    made quizQuestions.js and populated with questions in an array. Linked the JS file to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@
     function printNumber () {
             // selects the html element with id of dynamic-timer
         var divEl = document.querySelector("#dynamic-timer");
-            // creates variable timerEl and creates an html element with .createElement("span")
+            // creates variable timerNowEl and creates an html element with .createElement("span")
         var timerNowEl = document.createElement("span");
             // appends the innerHTML of divEl with the value of num
         divEl.innerHTML = num;
@@ -23,31 +23,35 @@
     // runs printNumber() every second
     let interval = setInterval(printNumber, 1000);
 
-    // Pseudocode of Remaining Functionality:
+// Pseudocode of Remaining Functionality:
 
-// 1. When I click the Start Button:
-    // a. Move to question page
-    // b. Start timer coutdown
-    // c. Dynamically generate questions from JS
-    // d. Dynamically generate answers from JS
+    // 1. When I click the Start Button:
+        // a. Move to question page
+        // b. Start timer coutdown
+        // c. Dynamically generate questions from JS
+        // d. Dynamically generate answers from JS
 
-// 2. When I click an answer: 
-    // a. Conditional statement that evaluates the selection
-        // if CORRECT, then:
-            // display 'Correct!'
-            // genterate next question
-        // if WRONG, then:
-            // display 'Wrong!'
-            // subtract time from clock
-// 3. When all quesitons are answered OR timer reaches 0:
-    // a. remove questions and buttons to be replaced with:
-        // -"All Done"
-        // -"Your final score is NUMBER HERE (remaining time)"
-        // -"Enter initials" in an input
-        // submit button 
-// 4. Submit button takes you to high score page which:
-    // a. displays all stored high scores in descending order, including your most recent score
-    // b. header and timer are gone
+    // 2. When I click an answer: 
+        // a. Conditional statement that evaluates the selection
+            // if CORRECT, then:
+                // display 'Correct!'
+                // genterate next question
+            // if WRONG, then:
+                // display 'Wrong!'
+                // subtract time from clock
+    // 3. When all quesitons are answered OR timer reaches 0:
+        // a. remove questions and buttons to be replaced with:
+            // -"All Done"
+            // -"Your final score is NUMBER HERE (remaining time)"
+            // -"Enter initials" in an input
+            // submit button 
+    // 4. Submit button takes you to high score page which:
+        // a. displays all stored high scores in descending order, including your most recent score
+        // b. header and timer are gone
+
+// array that will hold the questions:
+
+
 
 
 
